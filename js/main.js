9258948fa819f5f453249c1095631e635fee3bee
Sumer Mixon <sumer@ximm.us>
2017-09-12 12:12:25 -0400

    navigation and long hold added

---
+++ js/main.js
@@ -0,0 +1,24 @@
+// Define a click binding for all anchors in the page
+$( "a" ).on( "click", function( event ) {
+
+	// Prevent the usual navigation behavior
+	event.preventDefault();
+
+	// Alter the url according to the anchor's href attribute, and
+	// store the data-foo attribute information with the url
+	$.mobile.navigate( $(this).attr( "href" ), {
+		foo: $(this).attr("data-foo")
+	});
+
+	// Hypothetical content alteration based on the url. E.g, make
+	// an Ajax request for JSON data and render a template into the page.
+	// alterContent( $(this).attr("href") );
+});
+
+// Navigate to the data-log page on longhold of header bar.
+$(function(){
+  $( "header" ).bind( "taphold", tapholdHandler );
+  function tapholdHandler( event ){
+    $.mobile.navigate("#data-log");
+  }
+});
